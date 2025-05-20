a62f5e769176359527da003a99b8d18c9543ef1f
Myl-W <mylene.wan@gmail.com>
2025-05-20 15:59:56 +0200

    mise en place de la caméra, prise de photo et photo affichée dans camerascreen

--- screens/AddSignalement.jsx
+++ screens/AddSignalement.jsx
@@ -1,12 +1,25 @@
 import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import React from "react";
+import { Text, TouchableOpacity, Image } from "react-native";
+import { useSelector } from "react-redux";
 
 export default function AddSignalement({ navigation }) {
+
+  const photoUri = useSelector((state) => state.user.value.photo);
   return (
     <View style={styles.container}>
       <KeyboardAvoidingView
         style={styles.logContent}
         behavior={Platform.OS === "ios" ? "padding" : "height"}
-      ></KeyboardAvoidingView>
+      >
+          <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+            <Text>📸 Prendre une photo</Text>
+          </TouchableOpacity>
+
+          {photoUri && (
+            <Image source={{ uri: photoUri }} style={{ width: 200, height: 200 }} />
+          )}
+      </KeyboardAvoidingView>
     </View>
   );
 }
