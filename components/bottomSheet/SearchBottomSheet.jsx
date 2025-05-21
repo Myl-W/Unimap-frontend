7ab5d6f880cada1159f38f63a715fe5f3c41aa87
karel <buet.karel@gmail.com>
2025-05-21 17:56:52 +0200

    Déplacement du bouton filtre a gauche + Ajout de la suppression des recherches recentes

--- components/bottomSheet/SearchBottomSheet.jsx
+++ components/bottomSheet/SearchBottomSheet.jsx
@@ -20,149 +20,153 @@ import {
   suppRecentSearch,
 } from "../../reducers/trips";
 
-const SearchBottomSheet = forwardRef(({ handleSheetSearch, onTripReady }, ref) => {
-  const transport = useSelector((state) => state.trips.selectedTransport);
-  const dispatch = useDispatch();
-  const loc = useSelector((state) => state.trips.value);
-  const google = process.env.EXPO_PUBLIC_API_GOOGLE;
-  const [search, setSearch] = useState("");
-  const snapPoints = ["50%", "75%"];
-  const lastSearch = useSelector((state) => state.trips?.recentSearch);
+const SearchBottomSheet = forwardRef(
+  ({ handleSheetSearch, onTripReady }, ref) => {
+    const transport = useSelector((state) => state.trips.selectedTransport);
+    const dispatch = useDispatch();
+    const loc = useSelector((state) => state.trips.value);
+    const google = process.env.EXPO_PUBLIC_API_GOOGLE;
+    const [search, setSearch] = useState("");
+    const snapPoints = ["50%", "75%"];
+    const lastSearch = useSelector((state) => state.trips?.recentSearch);
 
-  //  -------- Fonction pour rechercher un itinéraire via l'API Google Directions ------------
-  const searchGoogle = () => {
-    fetch(
-      `https://maps.googleapis.com/maps/api/directions/json?origin=${loc.latitude},${loc.longitude}&destination=${search}&mode=${transport}&key=${google}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        // ------------- Adresse réelle de départ obtenue depuis l'API Google --------------
-        const arrival = data.routes[0].legs[0].end_address;
-        // ------------- Enregistrement dans l'historique des recherches -------------------
-        const searchRecent = {
-          arrival: arrival,
-        };
-        dispatch(recentSearch(searchRecent));
+    //  -------- Fonction pour rechercher un itinéraire via l'API Google Directions ------------
+    const searchGoogle = () => {
+      fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?origin=${loc.latitude},${loc.longitude}&destination=${search}&mode=${transport}&key=${google}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          // ------------- Adresse réelle de départ obtenue depuis l'API Google --------------
+          const arrival = data.routes[0].legs[0].end_address;
+          // ------------- Enregistrement dans l'historique des recherches -------------------
+          const searchRecent = {
+            arrival: arrival,
+          };
+          dispatch(recentSearch(searchRecent));
 
-        // ------------- Récupération et décodage de la route -------------------
-        const encodedPolyline = data.routes[0].overview_polyline.points;
-        const decodedPoints = polyline.decode(encodedPolyline);
-        const coords = decodedPoints.map((point) => ({
-          latitude: point[0],
-          longitude: point[1],
-        }));
-        //  ----------- Affiche la durée du trajet et le nombre de km  -------------
-        const tripTime = {
-          duration: data.routes[0].legs[0].duration.text,
-          distance: data.routes[0].legs[0].distance.text,
-        };
-        //  ----------- Mise à jour du réducer de la distance du trajet en heure/minutes et en km -------------
-        dispatch(setTripInfos(tripTime));
-        //  ----------- Mise à jour du réducer avec les coordonnées de la route -------------
-        dispatch(setRouteCoords(coords));
-        ref?.current?.close();
+          // ------------- Récupération et décodage de la route -------------------
+          const encodedPolyline = data.routes[0].overview_polyline.points;
+          const decodedPoints = polyline.decode(encodedPolyline);
+          const coords = decodedPoints.map((point) => ({
+            latitude: point[0],
+            longitude: point[1],
+          }));
+          //  ----------- Affiche la durée du trajet et le nombre de km  -------------
+          const tripTime = {
+            duration: data.routes[0].legs[0].duration.text,
+            distance: data.routes[0].legs[0].distance.text,
+          };
+          //  ----------- Mise à jour du réducer de la distance du trajet en heure/minutes et en km -------------
+          dispatch(setTripInfos(tripTime));
+          //  ----------- Mise à jour du réducer avec les coordonnées de la route -------------
+          dispatch(setRouteCoords(coords));
+          ref?.current?.close();
 
-        // ------------- Fermeture de la bottomSheet --------------------------
-      })
-      .catch((error) => {
-        console.error("Erreur lors de la récupération de la route :", error);
-      });
-  };
+          // ------------- Fermeture de la bottomSheet --------------------------
+        })
+        .catch((error) => {
+          console.error("Erreur lors de la récupération de la route :", error);
+        });
+    };
 
-  // Si une des valeurs change, on lance la fonction de recherche Google
-  // uniquement si le champ 'search' n'est pas vide (après suppression des espaces).
-  useEffect(() => {
-    if (search.trim() !== "") {
-      searchGoogle();
-    }
-  }, [transport]);
+    // Si une des valeurs change, on lance la fonction de recherche Google
+    // uniquement si le champ 'search' n'est pas vide (après suppression des espaces).
+    useEffect(() => {
+      if (search.trim() !== "") {
+        searchGoogle();
+      }
+    }, [transport]);
 
-  // fonction pour mapper le tableau afin d'afficher les elements dans le bottomSheet
-  const renderRecentSearch = () => {
-    return lastSearch?.map((item, index) => (
-      <View key={index} style={styles.historyAdress}>
-        <View style={styles.historyLign}>
-          <FontAwesome name="clock-o" size={24} color="black" />
-          <TouchableOpacity>
-            <FontAwesome name="heart" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text style={styles.addressHistory}>{item.arrival}</Text>
-        </View>
-        <TouchableOpacity onPress={() => handleDelete(index)}>
-          <FontAwesome
-            name="trash"
-            size={24}
-            color="black"
-            style={styles.iconDelete}
-          />
-        </TouchableOpacity>
-      </View>
-    ));
-  };
-
-  const handleDelete = (index) => {
-    const updatedSearch = [...lastSearch];
-    updatedSearch.splice(index, 1);
-    dispatch(suppRecentSearch(updatedSearch));
-  };
-
-  return (
-    <BottomSheetModal
-      ref={ref}
-      onChange={handleSheetSearch}
-      snapPoints={snapPoints}
-    >
-      <BottomSheetView style={styles.contentContainer}>
-        <View style={styles.buttonRow}>
-          <TouchableOpacity style={styles.optionButton}>
-            <View style={styles.optionButtonContent}>
-              <FontAwesome name="home" size={24} color="black" />
-              <Text style={styles.optionButtonText}>Domicile</Text>
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.optionButton}>
-            <View style={styles.optionButtonContent}>
-              <FontAwesome name="briefcase" size={24} color="black" />
-              <Text style={styles.optionButtonText}>Travail</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.searchInputContainer}>
-          <TouchableOpacity onPress={searchGoogle}>
-            <FontAwesome name="search" size={24} color="black" />
-          </TouchableOpacity>
-
-          <TextInput
-            style={styles.searchInput}
-            placeholder="Rechercher une adresse..."
-            value={search}
-            onChangeText={setSearch}
-            onFocus={() => {
-              ref.current?.present();
-              ref.current?.snapToIndex(2);
-              setSearch("");
-            }}
-          />
-
-          <TouchableOpacity>
-            <FontAwesome name="microphone" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
-
-        <ScrollView style={styles.addressHistoryContent}>
-          <View style={styles.scrollContent}>
-            <Text style={styles.historyTitle}>Adresses consultées :</Text>
-            {renderRecentSearch()}
+    // fonction pour mapper le tableau afin d'afficher les elements dans le bottomSheet
+    const renderRecentSearch = () => {
+      return lastSearch?.map((item, index) => (
+        <View key={index} style={styles.historyAdress}>
+          <View style={styles.historyLign}>
+            <FontAwesome name="clock-o" size={24} color="black" />
+            <TouchableOpacity>
+              <FontAwesome name="heart" size={22} color="black" />
+            </TouchableOpacity>
           </View>
-        </ScrollView>
-      </BottomSheetView>
-    </BottomSheetModal>
-  );
-});
+          <View>
+            <Text style={styles.addressHistory}>{item.arrival}</Text>
+          </View>
+          <TouchableOpacity onPress={() => handleDelete(index)}>
+            <FontAwesome
+              name="trash"
+              size={24}
+              color="black"
+              style={styles.iconDelete}
+            />
+          </TouchableOpacity>
+        </View>
+      ));
+    };
+
+    const handleDelete = (index) => {
+      const updatedSearch = [...lastSearch];
+      // Supprime l'élément à l'index spécifié
+      updatedSearch.splice(index, 1);
+      // Met à jour le store Redux avec le tableau modifié
+      dispatch(suppRecentSearch(updatedSearch));
+    };
+
+    return (
+      <BottomSheetModal
+        ref={ref}
+        onChange={handleSheetSearch}
+        snapPoints={snapPoints}
+      >
+        <BottomSheetView style={styles.contentContainer}>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.optionButton}>
+              <View style={styles.optionButtonContent}>
+                <FontAwesome name="home" size={24} color="black" />
+                <Text style={styles.optionButtonText}>Domicile</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.optionButton}>
+              <View style={styles.optionButtonContent}>
+                <FontAwesome name="briefcase" size={24} color="black" />
+                <Text style={styles.optionButtonText}>Travail</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.searchInputContainer}>
+            <TouchableOpacity onPress={searchGoogle}>
+              <FontAwesome name="search" size={24} color="black" />
+            </TouchableOpacity>
+
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Rechercher une adresse..."
+              value={search}
+              onChangeText={setSearch}
+              onFocus={() => {
+                ref.current?.present();
+                ref.current?.snapToIndex(2);
+                setSearch("");
+              }}
+            />
+
+            <TouchableOpacity>
+              <FontAwesome name="microphone" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <ScrollView style={styles.addressHistoryContent}>
+            <View style={styles.scrollContent}>
+              <Text style={styles.historyTitle}>Adresses consultées :</Text>
+              {renderRecentSearch()}
+            </View>
+          </ScrollView>
+        </BottomSheetView>
+      </BottomSheetModal>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   contentContainer: {
